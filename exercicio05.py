00bf6c3bdda0576569b46587366bd637fdd95c6c
Julia <julia.chdck@gmail.com>
2025-03-31 16:52:37 -0300

    Calcular media e se esta aprovado, reprovado ou em recuperacao

--- exercicio05.py
+++ exercicio05.py
@@ -5,6 +5,9 @@ nota3 = float(input("Terceira nota: "))
 media = (nota1 + nota2 + nota3)/3 # calculo da media do aluno
 # verificar se ele esta aprovado ou reprovado utilizando a media 7.0
 if media >= 7:
-    print("Aprovado")
+    print(f"Aprovado\nMédia: {media:.2f}")
 else:
-    print("Reprovado")
+    if media < 4:
+        print(f"Reprovado\nMédia: {media:.2f}")
+    else:
+        print(f"Recuperação\nMédia: {media:.2f}")
