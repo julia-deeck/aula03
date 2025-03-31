2164e95afbcec7fa95aa5b204d5872845ce209ff
Julia <julia.chdck@gmail.com>
2025-03-31 17:11:04 -0300

    Recebe dois times e resultado da partida

---
+++ exercicio06.py
@@ -0,0 +1,13 @@
+# receber o nome de 2 times
+time1 = input("Nome do primeiro time: ")
+time2 = input("Nome do segundo time: ")
+# número de gols marcados na partida (cada time)
+gol_time1 = int(input("Quantidade de gols do primeiro time: "))
+gol_time2 = int(input("Quantidade de gols do segundo time: "))
+
+if gol_time1 > gol_time2:
+    print(f"Vencedor da partida: {time1}")
+elif gol_time2 > gol_time1:
+    print(f"Vencedor da partida: {time2}")
+else:
+    print("EMPATE")
