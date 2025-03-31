fed9709897674b9f1e3159c793c54b5c20e67bd5
Julia <julia.chdck@gmail.com>
2025-03-31 15:59:09 -0300

    Trocar numeros de mesma variavel

---
+++ exercicio03.py
@@ -0,0 +1,12 @@
+# duas variaveis distintas A e B
+A = 5
+B = 10
+
+print(f" A {A}, B {B}")
+# algoritmo para efetuar a troca dos valores de forma que a variavel A possua o valor de B e vice-versa
+
+C = A
+A = B
+B = C
+
+print(f" A {A}, B {B}")
