8c76e5b35b96f5e7c9f4476c23eff224989123ef
Julia <julia.chdck@gmail.com>
2025-03-31 14:16:10 -0300

    Enviando exercicio01

---
+++ exercicio01.py
@@ -0,0 +1,5 @@
+# ler o nome de uma pessoa, a sua idade e o seu salário e mostrar as informações na tela
+nome = input("Qual seu nome?: ")
+idade = int(input("Qual sua idade?: "))
+salario = float(input("Qual seu salário?: "))
+print(f'Seu nome: {nome}\nSua idade: {idade}\nSeu salário: {salario}')
