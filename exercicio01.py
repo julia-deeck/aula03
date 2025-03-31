1f8396e461dfc1904c6b8f4845fa443d44a25fc5
Julia <julia.chdck@gmail.com>
2025-03-31 14:46:23 -0300

    Acrescimo de aumento percentual no salario

--- exercicio01.py
+++ exercicio01.py
@@ -2,4 +2,12 @@
 nome = input("Qual seu nome?: ")
 idade = int(input("Qual sua idade?: "))
 salario = float(input("Qual seu salário?: "))
+
+# o usuário digita o seu aumento percentual
+aumento = float(input("Digite o percentual de aumento de salário: "))
+percentual_salario = (salario * aumento)/100 # calculo do percentual
+salario_final = salario + percentual_salario # soma do salario com o percentual
+
 print(f'Seu nome: {nome}\nSua idade: {idade}\nSeu salário: {salario}')
+print(f'O percentual de aumento em reais foi de R${percentual_salario}'
+      f'\nO salário final é de R${salario_final}')
