a01839718451af999b93aade4cf09d9728fa8d4b
Julia <julia.chdck@gmail.com>
2025-04-02 14:56:22 -0300

    Receber numero e printar se ele e par ou impar

---
+++ exercicio08.py
@@ -0,0 +1,8 @@
+# receber um número e diga se ele é par ou ímpar
+
+num = int(input("Digite um número: "))
+
+if num % 2 == 0:
+    print(f"O número {num} é par")
+else:
+    print(f"O número {num} é ímpar")
