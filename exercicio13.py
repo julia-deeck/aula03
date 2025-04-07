c1f73ed28c98495bbf789786073a02ef5b1bb688
Julia <julia.chdck@gmail.com>
2025-04-07 16:45:37 -0300

    atualização do utilizando o for em tabuada de multiplicacao

--- exercicio13.py
+++ exercicio13.py
@@ -1,6 +1,5 @@
 # receber um numero inteiro e mostrar a tabuada de multiplicação desse numero
 num = int(input("Digite um número: "))
-tabuada = 0
 for x in range(1, 11):
     tabuada = num * x
     print(f"{num} x {x} = {tabuada}")
